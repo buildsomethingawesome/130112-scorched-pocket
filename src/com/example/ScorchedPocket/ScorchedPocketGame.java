964b6d7e6cc962ce1bc73fc022d1000a0954beb8
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-01-14 22:11:45 -0800

    Draw some graphics.
    Graphics are public domain:
    
    * [tank](http://openclipart.org/detail/141769/tank-by-wildchief) by David St. Clair
    * [explosion](http://openclipart.org/detail/40477/explosion-by-cybergedeon) by "dominiquechappard"
    * [molotov cocktail](http://openclipart.org/detail/4621/molotov-cocktail-by-ryanlerch) by Zeimusu, nicubunu
    * [nuclear explosion](http://openclipart.org/detail/166696/nuclear-explosion-by-tzunghaor) by "tzunghaor"

--- src/com/example/ScorchedPocket/ScorchedPocketGame.java
+++ src/com/example/ScorchedPocket/ScorchedPocketGame.java
@@ -5,18 +5,34 @@ import android.graphics.*;
 import android.view.View;
 
 public class ScorchedPocketGame extends View {
+    Bitmap backgroundImage;
+    Bitmap tank1Image;
+    Bitmap tank2Image;
+    Bitmap ammoImage;
+    Bitmap explosionImage;
 
     public ScorchedPocketGame(Context context) {
         super(context);
+
+        backgroundImage = BitmapFactory.decodeResource(getResources(),
+                R.drawable.map1);
+        tank1Image = BitmapFactory.decodeResource(getResources(),
+                R.drawable.tank1);
+        tank2Image = BitmapFactory.decodeResource(getResources(),
+                R.drawable.tank2);
+        ammoImage = BitmapFactory.decodeResource(getResources(),
+                R.drawable.ammo1);
+        explosionImage = BitmapFactory.decodeResource(getResources(),
+                R.drawable.explosion);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint redPaint = new Paint();
-        redPaint.setColor(Color.RED);
-        redPaint.setTextSize(20);
+        canvas.drawBitmap(backgroundImage, 0, 0, null);
 
-        canvas.drawText("Width: " + getWidth(), 100, 100, redPaint);
-        canvas.drawText("Height: " + getHeight(), 100, 150, redPaint);
+        canvas.drawBitmap(tank1Image, 11, 226, null);
+        canvas.drawBitmap(tank2Image, 404, 268, null);
+        canvas.drawBitmap(ammoImage, 85, 203, null);
+        canvas.drawBitmap(explosionImage, 155, 164, null);
     }
 }
