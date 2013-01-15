5d4de926ec4d26914166e0a07017ab11c341584d
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-01-14 22:10:59 -0800

    Draw something in our window and find out the screen resolution of our phone.

--- src/com/example/ScorchedPocket/ScorchedPocketGame.java
+++ src/com/example/ScorchedPocket/ScorchedPocketGame.java
@@ -9,4 +9,14 @@ public class ScorchedPocketGame extends View {
     public ScorchedPocketGame(Context context) {
         super(context);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint redPaint = new Paint();
+        redPaint.setColor(Color.RED);
+        redPaint.setTextSize(20);
+
+        canvas.drawText("Width: " + getWidth(), 100, 100, redPaint);
+        canvas.drawText("Height: " + getHeight(), 100, 150, redPaint);
+    }
 }
